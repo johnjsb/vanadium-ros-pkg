3b12ba3c6dd263d199b49851ef9d3e9cde551e80
MFergs7 <MFergs7@83ef6a08-d9a4-32ac-7ea9-9468f932a7fc>
2011-02-14 05:06:05 +0000

    Create 0.4.1 release

---
+++ 0.4.1/arbotix_controllers/nodes/traj_controller.py
@@ -0,0 +1,155 @@
+#!/usr/bin/env python
+
+"""
+  traj_controller.py - controls joints using trajectory msgs
+  Copyright (c) 2010-2011 Vanadium Labs LLC.  All right reserved.
+
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are met:
+      * Redistributions of source code must retain the above copyright
+        notice, this list of conditions and the following disclaimer.
+      * Redistributions in binary form must reproduce the above copyright
+        notice, this list of conditions and the following disclaimer in the
+        documentation and/or other materials provided with the distribution.
+      * Neither the name of Vanadium Labs LLC nor the names of its 
+        contributors may be used to endorse or promote products derived 
+        from this software without specific prior written permission.
+  
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  DISCLAIMED. IN NO EVENT SHALL VANADIUM LABS BE LIABLE FOR ANY DIRECT, INDIRECT,
+  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+  OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+  OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+"""
+import roslib; roslib.load_manifest('arbotix_controllers')
+import rospy
+import thread
+
+from trajectory_msgs.msg import JointTrajectory
+from sensor_msgs.msg import JointState
+from std_msgs.msg import Float64
+
+class Trajectory:
+    def __init__(self, names, positions, velocities, time):
+        self.names = names
+        self.positions = positions
+        self.velocities = velocities
+        self.time = time
+
+class TrajController:
+    """ Controller to handle trajectory-based servo control. """
+
+    def __init__(self):
+        rospy.init_node("traj_controller")
+        self.joints = rospy.get_param("~joints")
+        self.rate = rospy.get_param("~rate", 50)
+
+        self.mutex = thread.allocate_lock()
+
+        # joint values
+        self.valid = False
+        self.dirty = False
+        self.current = [0.0 for i in self.joints]
+        self.desired = [0.0 for i in self.joints]   
+        self.velocity = [0.0 for i in self.joints]   
+        self.trajectories = list()      
+
+        # subscriptions
+        rospy.Subscriber('~command', JointTrajectory, self.cmdTrajCb)
+        rospy.Subscriber('joint_states', JointState, self.stateCb)
+        self.publishers = dict(zip(self.joints, [rospy.Publisher(name+"/command", Float64) for name in self.joints]))
+        
+        rospy.loginfo("Started joint_controller controlling: " + str(self.joints))
+
+        rate = rospy.Rate(self.rate)
+        while not rospy.is_shutdown():
+            # grab mutex
+            self.mutex.acquire()  
+            # remove missed frames
+            now = rospy.Time.now()
+            while len(self.trajectories) > 1 and self.trajectories[0].time < now:
+                print "loop: deleting trajectory"
+                del self.trajectories[0]
+            # update desired/velocity
+            if self.trajectories:
+                if not self.trajectories[0].time > now + rospy.Duration(1/self.rate):
+                    # copy trajectory to desired/velocity arrays
+                    traj = self.trajectories[0]
+                    try:
+                        indexes = [traj.names.index(name) for name in self.joints]
+                    except ValueError as val:
+                        rospy.logerr('Invalid joint state message.')
+                        return           
+                    self.desired = [ traj.positions[k] for k in indexes ]
+                    self.velocity = [ traj.velocities[k] for k in indexes ]
+                    del self.trajectories[0]                 
+                    self.dirty = True
+            if self.dirty and self.valid:
+                # interpolate and update outputs
+                cumulative = 0.0
+                err = [ (d-c) for d,c in zip(self.desired,self.current)]
+                for i in range(len(self.joints)):
+                    cmd = err[i]
+                    cumulative += cmd
+                    if self.velocity[i] != 0:
+                        top = self.velocity[i]/self.rate
+                        if cmd > top:
+                            cmd = top
+                        elif cmd < -top:
+                            cmd = -top
+                    self.current[i] += cmd
+                    self.publishers[self.joints[i]].publish(Float64(self.current[i]))
+                # are we done?
+                if cumulative == 0.0:
+                    self.dirty = False   
+            # release mutex
+            self.mutex.release()       
+            rate.sleep()
+
+    def restart(self):
+        """ Restart the controller, by clearing out trajectory lists. """
+        self.trajectories = list()    
+        rospy.loginfo("Aborting trajectory, restart")
+
+    def cmdTrajCb(self, msg):
+        """ Store trajectories. """
+        # grab mutex
+        self.mutex.acquire()  
+        # Stop?
+        if len(msg.points) == 0:
+            self.restart()
+        else:
+            # find start time of this trajectory set
+            start = msg.header.stamp + msg.points[0].time_from_start
+            # remove trajectories after start
+            while self.trajectories and self.trajectories[-1].time > start:
+                rospy.loginfo("Delete trajectory at "+str(self.trajectories[-1].time))
+                del self.trajectories[-1]
+            # fo' each point in trajectory
+            for point in msg.points:
+                # find start time of point
+                t = msg.header.stamp + point.time_from_start
+                self.trajectories.append(Trajectory(msg.joint_names, point.positions, point.velocities, t))
+        # release mutex
+        self.mutex.release()
+
+    def stateCb(self, msg):
+        """ The callback that converts JointState into servo position for interpolation. """
+        if self.dirty and self.valid:
+            return
+        try:
+            indexes = [msg.name.index(name) for name in self.joints]
+        except ValueError as val:
+            self.valid = False
+            rospy.logerr('Invalid joint state message.')
+            return
+        self.current = [ msg.position[k] for k in indexes ]
+        self.valid = True
+
+if __name__=="__main__":
+    tc = TrajController()
---
+++ 0.4.1/arbotix_firmware/src/ros.h
@@ -0,0 +1,113 @@
+/* 
+  Common Definitions for ROS driver ArbotiX Firmware
+  Copyright (c) 2008-2010 Vanadium Labs LLC.  All right reserved.
+ 
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are met:
+      * Redistributions of source code must retain the above copyright
+        notice, this list of conditions and the following disclaimer.
+      * Redistributions in binary form must reproduce the above copyright
+        notice, this list of conditions and the following disclaimer in the
+        documentation and/or other materials provided with the distribution.
+      * Neither the name of the Vanadium Labs LLC nor the names of its 
+        contributors may be used to endorse or promote products derived 
+        from this software without specific prior written permission.
+  
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  DISCLAIMED. IN NO EVENT SHALL VANADIUM LABS BE LIABLE FOR ANY DIRECT, INDIRECT,
+  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+  OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+  OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/ 
+
+/* ArbotiX (id:253) Instruction Definitions */
+#define ARB_SIZE_POSE       7    // pose size: a single param for size of pose
+#define ARB_LOAD_POSE       8    // load pose: index, then pose positions (# of params = 2*pose_size)
+#define ARB_LOAD_SEQ        9    // seq size: a single param for the size of the seq
+#define ARB_PLAY_SEQ        10   // load seq: index/times (# of params = 3*seq_size)
+#define ARB_LOOP_SEQ        11   // play seq: no params
+#define ARB_TEST            25   // hardware test: no params
+#define ARB_SYNC_READ       0x84
+
+/* ArbotiX (id:253) Register Table Definitions */
+#define REG_MODEL_NUMBER_L  0
+#define REG_MODEL_NUMBER_H  1
+#define REG_VERSION         2
+#define REG_ID              3
+#define REG_BAUD_RATE       4
+#define REG_DIGITAL         5   // First block of digital pins to read
+                                // base + index, bit 1 = value (0,1), bit 0 = direction (0,1)
+#define REG_RESCAN          15
+#define REG_RETURN_LEVEL    16
+#define REG_ALARM_LED       17
+#define REG_ANA_BASE        18  // First Analog Port
+#define REG_SERVO_BASE      26  // Up to 10 servos, each uses 2 bytes (L, then H), pulse width (0, 1000-2000ms)
+#define REG_MOVING          46
+#define REG_LM_SIGN         47  // Raw motor pwm (-255 to 255), 1 byte sign + 1 byte speed per side
+#define REG_LM_PWM          48
+#define REG_RM_SIGN         49
+#define REG_RM_PWM          50
+
+#define REG_LM_SPEED_L      51  // Motor Speed (ticks/sec, 2 bytes, signed)
+#define REG_LM_SPEED_H      52
+#define REG_RM_SPEED_L      53
+#define REG_RM_SPEED_H      54
+
+#define REG_X_SPEED_L       51  // OR Nuke Speed (mm/s, 2 bytes, signed) 
+#define REG_X_SPEED_H       52
+#define REG_R_SPEED_L       53
+#define REG_R_SPEED_H       54
+#define REG_Y_SPEED_L       55
+#define REG_Y_SPEED_H       56
+
+#define REG_ENC_LEFT_L      57  // Current Encoder Values (ticks, 4 bytes, signed)    
+#define REG_ENC_RIGHT_L     61
+
+#define REG_ENC_X_L         57  // OR Nuke Encoder Values (mm, 4 bytes, signed)
+#define REG_ENC_R_L         61
+#define REG_ENC_Y_L         65
+
+#define REG_KP              69  // PID parameters ... not currently implemented
+#define REG_KD              70
+#define REG_KI              71
+#define REG_KO              72
+
+#define REG_PML_SERVO       80  // ID for PML servo
+#define REG_PML_SENSOR      81  // ID for PML sensor
+#define REG_STEP_START_L    82  // minimum ticks, low byte
+#define REG_STEP_START_H    83
+#define REG_STEP_VALUE      84  // ticks per step
+#define REG_STEP_COUNT      85  // number of steps
+#define REG_PML_ENABLE      86  // scan or no?
+
+#define REG_PML_DIR         88  // direction of scan last taken
+#define REG_PML_TIME_L      89  // low byte of time offset from first read on this scan
+#define REG_PML_TIME_H      90  // high byte of time offset from first read on this scan
+#define REG_PML_BASE        91  // start of data bytes for this scan
+
+/* Packet Decoding */
+int mode = 0;                   // where we are in the frame
+
+unsigned char id = 0;           // id of this frame
+unsigned char length = 0;       // length of this frame
+unsigned char ins = 0;          // instruction of this frame
+
+unsigned char params[143];      // parameters (match RX-64 buffer size)
+unsigned char index = 0;        // index in param buffer
+
+#ifdef USE_PML
+int scan_dir;
+int step_start;
+int step_value;
+#endif
+
+int checksum;                   // checksum
+
+#define INSTRUCTION_ERROR   0x40
+#define CHECKSUM_ERROR      0x10
+
