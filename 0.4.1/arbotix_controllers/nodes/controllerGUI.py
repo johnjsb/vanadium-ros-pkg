51e8624f4bcb5b1bfa252c8235b58c8e9728453b
MFergs7 <MFergs7@83ef6a08-d9a4-32ac-7ea9-9468f932a7fc>
2011-02-18 14:42:58 +0000

    (arbotix_controllers) fix manifest of old-style controllerGUI

--- 0.4.1/arbotix_controllers/nodes/controllerGUI.py
+++ 0.4.1/arbotix_controllers/nodes/controllerGUI.py
@@ -21,7 +21,7 @@
   Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 """
 
-import roslib; roslib.load_manifest('arbotix_controller')
+import roslib; roslib.load_manifest('arbotix_controllers')
 import rospy
 import wx
 
