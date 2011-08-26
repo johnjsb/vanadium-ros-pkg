20872510f8f132a6cba951d33ca0b15075009f25
MFergs7 <MFergs7@83ef6a08-d9a4-32ac-7ea9-9468f932a7fc>
2011-08-26 07:01:16 +0000

    (hippo_teleop) first checkin

---
+++ sandbox/hippo_teleop/src/teleop_joy.cpp
@@ -0,0 +1,116 @@
+/*
+ * Copyright (c) 2010, Willow Garage, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Willow Garage, Inc. nor the names of its
+ *       contributors may be used to endorse or promote products derived from
+ *       this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <ros/ros.h>
+#include <geometry_msgs/Twist.h>
+#include <std_msgs/Float64.h>
+#include <joy/Joy.h>
+#include "boost/thread/mutex.hpp"
+#include "boost/thread/thread.hpp"
+#include "ros/console.h"
+
+class TurtlebotTeleop
+{
+public:
+  TurtlebotTeleop();
+
+private:
+  void joyCallback(const joy::Joy::ConstPtr& joy);
+  void publish();
+
+  ros::NodeHandle ph_, nh_;
+
+  int linear_, angular_, gripper_, deadman_axis_;
+  double l_scale_, a_scale_, g_scale_;
+  ros::Publisher vel_pub_;
+  ros::Publisher gripper_pub_;
+  ros::Subscriber joy_sub_;
+
+  geometry_msgs::Twist last_published_;
+  std_msgs::Float64 last_gripper_;
+  boost::mutex publish_mutex_;
+  bool deadman_pressed_;
+  ros::Timer timer_;
+};
+
+TurtlebotTeleop::TurtlebotTeleop():
+  ph_("~"),
+  linear_(1),
+  angular_(0),
+  gripper_(2),
+  deadman_axis_(4),
+  l_scale_(0.3),
+  a_scale_(0.9),
+  g_scale_(1.0)
+{
+  ph_.param("axis_linear", linear_, linear_);
+  ph_.param("axis_angular", angular_, angular_);
+  ph_.param("axis_gripper", gripper_, gripper_);
+  ph_.param("axis_deadman", deadman_axis_, deadman_axis_);
+  ph_.param("scale_angular", a_scale_, a_scale_);
+  ph_.param("scale_linear", l_scale_, l_scale_);
+  ph_.param("scale_gripper", g_scale_, g_scale_);
+
+  vel_pub_ = nh_.advertise<geometry_msgs::Twist>("turtlebot_node/cmd_vel", 1);
+  gripper_pub_ = nh_.advertise<std_msgs::Float64>("gripper_joint/command", 1);
+  joy_sub_ = nh_.subscribe<joy::Joy>("joy", 10, &TurtlebotTeleop::joyCallback, this);
+
+  timer_ = nh_.createTimer(ros::Duration(0.1), boost::bind(&TurtlebotTeleop::publish, this));
+}
+
+void TurtlebotTeleop::joyCallback(const joy::Joy::ConstPtr& joy)
+{ 
+  geometry_msgs::Twist vel;
+  vel.angular.z = a_scale_*joy->axes[angular_];
+  vel.linear.x = l_scale_*joy->axes[linear_];
+  last_published_ = vel;
+
+  last_gripper_.data = g_scale_*joy->axes[gripper_];
+  
+  deadman_pressed_ = joy->buttons[deadman_axis_];
+}
+
+void TurtlebotTeleop::publish()
+{
+  boost::mutex::scoped_lock lock(publish_mutex_);
+
+  if (deadman_pressed_)
+  {
+    vel_pub_.publish(last_published_);
+    gripper_pub_.publish(last_gripper_);
+  }
+
+}
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "teleop_joystick");
+  TurtlebotTeleop turtlebot_teleop;
+
+  ros::spin();
+}
