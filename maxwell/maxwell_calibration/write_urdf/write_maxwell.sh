b7e98e560f91e0264439b740ffe503b064c5f0d8
MFergs7 <MFergs7@83ef6a08-d9a4-32ac-7ea9-9468f932a7fc>
2011-11-10 00:21:28 +0000

    (maxwell_calibration) add new package, still in testing

---
+++ maxwell/maxwell_calibration/write_urdf/write_maxwell.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+original_system=`rospack find maxwell_calibration`/estimate_params/config/system.yaml
+final_system=/tmp/maxwell_calibration/system_calibrated.yaml
+bag_in=/tmp/maxwell_calibration/cal_measurements.bag
+urdf_out=robot_calibrated.xml
+`./update_maxwell_urdf.py $original_system $final_system $bag_in $urdf_out > /tmp/maxwell_calibration/urdf_writer_debug.log
+if [ $? = 0 ]
+then
+    echo "Wrote new URDF to $urdf_out"
+else
+    echo "Error writing URDF"
+fi
+
