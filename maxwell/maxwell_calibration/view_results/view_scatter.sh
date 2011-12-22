e59a584f0f4e410b7b3de7492bfd3af527354f94
MFergs7 <MFergs7@83ef6a08-d9a4-32ac-7ea9-9468f932a7fc>
2011-12-22 19:30:56 +0000

    (maxwell_calibration) update to new error visualizations

--- maxwell/maxwell_calibration/view_results/view_scatter.sh
+++ maxwell/maxwell_calibration/view_results/view_scatter.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-rosrun calibration_estimation post_process.py /tmp/maxwell_calibration/cal_measurements.bag /tmp/maxwell_calibration/ `rospack find maxwell_calibration`/view_results/scatter_config.yaml
+rosrun calibration_estimation error_visualization.py /tmp/maxwell_calibration/cal_measurements.bag /tmp/maxwell_calibration/ `rospack find maxwell_calibration`/view_results/scatter_config.yaml
